144d7cc482ec03ce0367db55b5ddde2ee6fd4a2f
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2012-07-23 17:49:50 +0300

    Utility class is added

---
+++ models/utility.js
@@ -0,0 +1,19 @@
+function Utility () {};
+
+Utility.prototype.sendMessageToAllPlayers = function(message,socket,players) {
+	
+};
+
+Utility.prototype.sendMessageToSpecificPlayer = function(message,socket,player) {
+	
+};
+
+Utility.prototype.sendMessageToTable = function(message,socket,table) {
+	
+};
+
+Utility.prototype.sendMessageToAllFreePlayers = function(message,socket,players) {
+
+};
+
+module.exports = Utility;
